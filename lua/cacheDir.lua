90adb96c0b18cf29ba906de2b753f6a932d97d40
yixiaobai <yixiaobai.lp@qq.com>
2021-12-13 17:41:19 +0800

    方法格式修改

--- lua/cacheDir.lua
+++ lua/cacheDir.lua
@@ -5,21 +5,29 @@
 local _M = {};
 -- 获取系统名称
 local osName = vim.loop.os_uname().sysname;
+-- nvim的配置路径
+local vimPath = vim.fn.stdpath('config');
 ---------------------------------------------------------------------------------------
-function _M:get()
-    self.isMac = osName == 'Darwin';
-    self.isLinux = osName == 'Linux';
-    self.isWindows = osName == 'Windows_NT';
-    self.vimPath = vim.fn.stdpath('config');
-    local pathSep = self.isWindows and '\\' or '/';
-    local home = self.isWindows and os.getenv('USERPROFILE') or os.getenv("HOME");
-    self.cacheDir = home .. pathSep .. '.cache' .. pathSep .. 'nvim' .. pathSep;
-    self.modulesDir = self.vimPath .. pathSep .. 'modules';
-    self.home = home;
-    self.data_dir = string.format('%s/site/', vim.fn.stdpath('data'));
+function _M.get()
+	local dirs = {};
+	dirs = setCacheDir(dirs);
+	return dirs;
+end
 
-    return self;
+-- 设置缓存以及备份目录
+function setCacheDir(dirs)
+    dirs.isMac = osName == 'Darwin';
+    dirs.isLinux = osName == 'Linux';
+    dirs.isWindows = osName == 'Windows_NT';
+    dirs.vimPath = vimPath;
+    local pathSep = isWindows and '\\' or '/';
+    local home = isWindows and os.getenv('USERPROFILE') or os.getenv("HOME");
+    dirs.cacheDir = home .. pathSep .. '.cache' .. pathSep .. 'nvim' .. pathSep;
+    dirs.modulesDir = vimPath .. pathSep .. 'modules';
+    dirs.home = home;
+    dirs.data_dir = string.format('%s/site/', vim.fn.stdpath('data'));
+	return dirs;
 end
 ---------------------------------------------------------------------------------------
 
-return _M:get();
+return _M.get();
