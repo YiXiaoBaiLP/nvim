aa446437c886ece10862b056f90fce277b6436df
YiXiaoBai <yixiaobai.lp@qq.com>
2022-02-09 12:53:26 +0800

    调用新的options配置文件

--- init.lua
+++ init.lua
@@ -7,35 +7,8 @@
   ╚═╝     ╚═╝   ╚═╝       ╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝ ╚═════╝
 --]=]
 
--- nvim路径相关
-local cache = require("core.CacheDir");
 -- 设置nvim
-require("core.Options");
--- Pack包管理器
-local pack = require("core.Pack");
--- nvim按键设置
-require("core.Mapping");
--- nvim 事件
-local event = require("core.Event");
+local options = require("core.init-options");
 
--- vimscript中的全局编辑器变量
-local g = vim.g;
--- 一次执行多行vimscript命令
-local cmd = vim.cmd;
---vim.loop 是暴露LibUV接口模块
-local loop = vim.loop;
--- vim.o.{option}：全局选项
--- vim.bo.{option}：buffer-local选项
--- vim.wo.{option}：window-local选项
-local o, wo, bo = vim.o, vim.wo, vim.bo;
 
--- 获取home路径
-local home_dir = loop.os_homedir();
-
--- 创建缓存文件夹
-cache.createDirs();
-
-pack.ensurePlugins();
-pack.loadCompile();
-
-event.loadAutoCmds();
+options.LoadOptions();
