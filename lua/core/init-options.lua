d1ac0d1c318b9055e8fa964f7e857a0dd4ba17bd
YiXiaoBai <yixiaobai.lp@qq.com>
2022-02-10 12:33:49 +0800

    Update 添加一些基础配置比如：动态显示行号等

--- lua/core/init-options.lua
+++ lua/core/init-options.lua
@@ -21,8 +21,29 @@ function M.LoadOptions()
 	go.visualbell = true;
 	-- 提示缓冲区已经修改，但文件尚未保存
 	go.hidden = true;
+	-- 允许可视列块的虚拟编辑
+	go.virtualedit = "block";
+	-- 当backup 与 writebackup 同时为 false时，没有备份文件
+	-- 关闭文件备份
+	go.backup = false;
+	-- 覆盖文件前建立备份，文件写入成功后，除非 backup 打开，否则删除该备份
+	go.writebackup = false;
+	-- 关闭缓冲区使用交换文件（关闭后不要在大文件，无法恢复）
+	go.swapfile = false;
+	-- 使用系统剪贴版
+    go.clipboard = "unnamedplus";
+	-- 修复索引线
+	o.colorcolumn = "99999";
+	-- so that `` is visible in markdown files
+	o.conceallevel = 0;
+	-- 设置历史操作记录为2000条
+	o.history = 2000;
+	-- 给出换行符的格式（<EOL>)
+	o.fileformats = "unix,dos,mac";
 	-- 显示行号
 	o.number = true;
+	-- 相对的显示行号
+	o.relativenumber = true;
 
 end
 
