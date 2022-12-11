00262b22c0b16717e955456daf648ef328d81796
燚小白 <yixiaobai.lp@qq.com>
2022-12-11 22:51:33 +0800

    插件下载模式修改

--- lua/core/pack.lua
+++ lua/core/pack.lua
@@ -67,7 +67,7 @@ function M:load_packer()
 		max_jobs = 20,
 		display = {
 			open_fn = function()
-				return require("packer.util").float({ border = "none" })
+				return require("packer.util").float({ border = "rounded" })
 			end,
 		},
 	})
